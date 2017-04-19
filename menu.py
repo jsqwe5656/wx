816b410a3e935242702d6d2ba190f681bbe93e5c
zbf <516845590@qq.com>
2017-04-19 14:04:11 +0800

    提交菜单

--- menu.py
+++ menu.py
@@ -35,40 +35,31 @@ if __name__ =='__main__':
     myMenu = Menu()
     postJson = """
     {
-        "button":
-        [
-            {
-                "type": "click",
-                "name": "开发指引",
-                "key":  "mpGuide"
-            },
-            {
-                "name": "公众平台",
-                "sub_button":
-                [
-                    {
-                        "type": "view",
-                        "name": "更新公告",
-                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
-                    },
-                    {
-                        "type": "view",
-                        "name": "接口权限说明",
-                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
-                    },
-                    {
-                        "type": "view",
-                        "name": "返回码说明",
-                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433747234&token=&lang=zh_CN"
-                    }
-                ]
-            },
-            {
-                "type": "media_id",
-                "name": "旅行",
-                "media_id": "z2zOokJvlzCXXNhSjF46gdx6rSghwX2xOD5GUV9nbX4"
-            }
-          ]
-    }
+    "button": [
+        {
+            "type": "click", 
+            "name": "今日歌曲", 
+            "key": "V1001_TODAY_MUSIC"
+        }, 
+        {
+            "type": "click", 
+            "name": "歌手简介", 
+            "key": "V1001_TODAY_SINGER"
+        }, 
+        {
+            "name": "菜单", 
+            "sub_button": [
+                {
+                    "type": "click", 
+                    "name": "hello word", 
+                    "key": "V1001_HELLO_WORLD"
+                }, 
+                {
+                    "type": "click", 
+                    "name": "赞一下我们", 
+                    "key": "V1001_GOOD"
+                }
+            ]
+        }
     """
     myMenu.create(postJson)
